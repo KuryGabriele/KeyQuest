f6bf1da178204d38fb62a066261be2558e567806
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-03 18:58:26 +0200

    More work on Level select screen

--- app/src/main/java/com/kuricki/keyquest/ui/views/LevelSelectScreen.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/views/LevelSelectScreen.kt
@@ -1,7 +1,13 @@
 package com.kuricki.keyquest.ui.views
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,8 +16,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kuricki.keyquest.R
+import com.kuricki.keyquest.data.GameLevel
 import com.kuricki.keyquest.data.LevelSelectViewModel
 
 @Composable
@@ -26,14 +34,52 @@ fun LevelSelectScreen(
     ) {
         Text(
             text = stringResource(R.string.greeting) + " " + lsUiState.userName,
-            style = MaterialTheme.typography.headlineLarge,
+            style = MaterialTheme.typography.displayLarge,
             modifier = modifier
                 .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
         )
+        Spacer(modifier = Modifier.height(32.dp))
         Column(
-            modifier = modifier,
+            modifier = modifier
+                .align(Alignment.CenterHorizontally),
+            verticalArrangement = Arrangement.Center,
         ) {
+            lsUiState.levels.forEach { lvl ->
+                LevelCard(modifier, levelSelectViewModel, lvl)
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+}
 
+@Composable
+fun LevelCard(
+    modifier: Modifier,
+    levelSelectViewModel: LevelSelectViewModel = viewModel(),
+    lvl: GameLevel
+) {
+    Card(
+        modifier = modifier
+            .size(350.dp, 150.dp)
+            .clickable { }
+    ) {
+        Column (
+            modifier = modifier
+        ) {
+            Text(
+                text = lvl.displayName,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = modifier
+                    .padding(16.dp)
+            )
+            Text(
+                text = stringResource(R.string.highScore) + lvl.bestScore + "/100",
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = modifier
+                    .padding(16.dp)
+            )
         }
     }
 }
