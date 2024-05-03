09a4326b36b139e197f65dfbc6d36d4f3aa574f0
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-03 19:06:44 +0200

    Made level cards elevedated

--- app/src/main/java/com/kuricki/keyquest/data/LevelSelectUiState.kt
+++ app/src/main/java/com/kuricki/keyquest/data/LevelSelectUiState.kt
@@ -4,6 +4,10 @@ data class LevelSelectUiState(
     val userName: String = "Kury",
     val levels: MutableList<GameLevel> = (mutableListOf(
         GameLevel(0, "Lvl1: Ode to joy!", 0,0, 0),
+        GameLevel(1, "Lvl2: Ode to joy!", 0,0, 0),
+        GameLevel(0, "Lvl1: Ode to joy!", 0,0, 0),
+        GameLevel(1, "Lvl2: Ode to joy!", 0,0, 0),
+        GameLevel(0, "Lvl1: Ode to joy!", 0,0, 0),
         GameLevel(1, "Lvl2: Ode to joy!", 0,0, 0)
     ))
 )
--- app/src/main/java/com/kuricki/keyquest/ui/views/LevelSelectScreen.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/views/LevelSelectScreen.kt
@@ -7,7 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -59,7 +60,10 @@ fun LevelCard(
     levelSelectViewModel: LevelSelectViewModel = viewModel(),
     lvl: GameLevel
 ) {
-    Card(
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
         modifier = modifier
             .size(350.dp, 150.dp)
             .clickable { }
