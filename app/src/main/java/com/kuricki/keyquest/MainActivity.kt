9c399a52efa4a67aa4841439405d9bc454f84f1a
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-03 17:16:18 +0200

    Cleanup

--- app/src/main/java/com/kuricki/keyquest/MainActivity.kt
+++ app/src/main/java/com/kuricki/keyquest/MainActivity.kt
@@ -3,10 +3,6 @@ package com.kuricki.keyquest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.kuricki.keyquest.ui.theme.KeyQuestTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,21 +13,5 @@ class MainActivity : ComponentActivity() {
                 KeyQuestApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KeyQuestTheme {
-        Greeting("Android")
     }
 }
