0d089951f7147df9f32219fc3a7a25dd87f611bb
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-03 13:49:03 +0200

    Added register button in login screen

--- app/src/main/java/com/kuricki/keyquest/ui/views/LoginScreen.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/views/LoginScreen.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,9 +12,6 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -24,7 +19,6 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kuricki.keyquest.R
-import com.kuricki.keyquest.data.LoginUiState
 import com.kuricki.keyquest.ui.LoginViewModel
 
 @Composable
@@ -74,6 +68,25 @@ fun LoginScreen(
                     stringResource(R.string.login),
                     style = MaterialTheme.typography.bodyLarge
                     )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(R.string.or),
+                style = MaterialTheme.typography.labelLarge,
+                modifier = modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                modifier = modifier
+                    .width(120.dp)
+                    .height(50.dp)
+                    .align(Alignment.CenterHorizontally),
+                onClick = { /*TODO*/ }) {
+                Text(
+                    stringResource(R.string.register),
+                    style = MaterialTheme.typography.bodyLarge
+                )
             }
         }
     }
