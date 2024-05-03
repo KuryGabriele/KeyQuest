6a340d2a7ccc76da2b1151d6585eba09ec1d4d31
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-03 13:48:49 +0200

    Added retrofit dependecies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,11 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //retrofit
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
 }
