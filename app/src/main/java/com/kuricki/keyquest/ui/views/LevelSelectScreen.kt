8a084d559f98622d8085ae364f52ef43d5055bfb
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-14 13:49:52 +0200

    More work on level select

--- app/src/main/java/com/kuricki/keyquest/ui/views/LevelSelectScreen.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/views/LevelSelectScreen.kt
@@ -3,10 +3,12 @@ package com.kuricki.keyquest.ui.views
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
@@ -16,6 +18,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -65,11 +68,12 @@ fun LevelCard(
             defaultElevation = 6.dp
         ),
         modifier = modifier
-            .size(350.dp, 150.dp)
+            .width(350.dp)
             .clickable { }
     ) {
-        Column (
-            modifier = modifier
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = lvl.displayName,
@@ -77,13 +81,37 @@ fun LevelCard(
                 modifier = modifier
                     .padding(16.dp)
             )
-            Text(
-                text = stringResource(R.string.highScore) + lvl.bestScore + "/100",
-                style = MaterialTheme.typography.labelLarge,
-                color = MaterialTheme.colorScheme.primary,
-                modifier = modifier
-                    .padding(16.dp)
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(
+                    text = stringResource(R.string.highScore) + lvl.bestScore + "/100",
+                    style = MaterialTheme.typography.labelLarge,
+                    color = MaterialTheme.colorScheme.primary,
+                )
+                if(lvl.difficulty == 0) {
+                    Text(
+                        text = stringResource(R.string.easy),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.Green,
+                    )
+                } else if(lvl.difficulty == 1) {
+                    Text(
+                        text = stringResource(R.string.medium),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.Yellow,
+                    )
+                } else {
+                    Text(
+                        text = stringResource(R.string.hard),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = Color.Red,
+                    )
+                }
+            }
         }
     }
 }
