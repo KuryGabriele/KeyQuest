fae7338bbb44d534fec8cf4635e2d60ffc0ace49
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-18 20:39:01 +0200

    Added pressed notes buffer to show all notes currently pressed

--- app/src/main/java/com/kuricki/keyquest/midiStuff/MyMidiReceiver.kt
+++ app/src/main/java/com/kuricki/keyquest/midiStuff/MyMidiReceiver.kt
@@ -3,14 +3,21 @@ package com.kuricki.keyquest.midiStuff
 import android.media.midi.MidiReceiver
 
 class MyMidiReceiver:MidiReceiver() {
+    var keysPressedCurrently = mutableSetOf<String>()
     override fun onSend(data: ByteArray, offset: Int, count: Int, timestamp: Long) {
         // Process the received MIDI data
         // For example, print the MIDI data to the log
         val receivedData = data.copyOfRange(offset, offset + count)
         //check if receivedData[1] is not null
-        if(receivedData.size > 1 && receivedData[2] > 0) {
+        //println("MIDI Data Received: ${receivedData.joinToString(", ")}")
+        if(receivedData[0].toInt() == -112 && receivedData.size > 1 && receivedData[2] >= 0) {
             val note = MidiToNote(receivedData[1].toInt())
-            println("MIDI Data Received: $note")
+            if(receivedData[2] > 0) {
+                keysPressedCurrently.add(note)
+            } else {
+                keysPressedCurrently.remove(note)
+            }
+            println("Keys Pressed: $keysPressedCurrently")
         }
     }
 }
