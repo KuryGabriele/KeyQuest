8635697548852d71c136a005f6b8974daf1de69a
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-18 20:25:13 +0200

    Added MidiToNote and NoteToMidi converters

---
+++ app/src/main/java/com/kuricki/keyquest/midiStuff/MidiNotesConverter.kt
@@ -0,0 +1,41 @@
+package com.kuricki.keyquest.midiStuff
+
+fun MidiToNote(midi: Int): String {
+    val note = midi % 12
+    val octave = midi / 12 - 1
+    var noteName = when (note) {
+        0 -> "C"
+        1 -> "C#"
+        2 -> "D"
+        3 -> "D#"
+        4 -> "E"
+        5 -> "F"
+        6 -> "F#"
+        7 -> "G"
+        8 -> "G#"
+        9 -> "A"
+        10 -> "A#"
+        11 -> "B"
+        else -> ""
+    }
+
+    return "$noteName$octave"
+}
+
+fun NoteToMidi(note: String): Int {
+    return when (note) {
+        "C" -> 0
+        "C#" -> 1
+        "D" -> 2
+        "D#" -> 3
+        "E" -> 4
+        "F" -> 5
+        "F#" -> 6
+        "G" -> 7
+        "G#" -> 8
+        "A" -> 9
+        "A#" -> 10
+        "B" -> 11
+        else -> -1
+    }
+}
