63cd9e6556dcc859e1f58ac283006050190d58eb
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-18 17:28:18 +0200

    Now app prints incoming midi inputs

--- app/src/main/java/com/kuricki/keyquest/MainActivity.kt
+++ app/src/main/java/com/kuricki/keyquest/MainActivity.kt
@@ -3,20 +3,22 @@ package com.kuricki.keyquest
 import android.content.Context
 import android.media.midi.MidiDevice
 import android.media.midi.MidiDeviceInfo
-import android.media.midi.MidiInputPort
 import android.media.midi.MidiManager
+import android.media.midi.MidiOutputPort
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
+import com.kuricki.keyquest.midiStuff.MyMidiReceiver
 import com.kuricki.keyquest.ui.theme.KeyQuestTheme
 
 class MainActivity : ComponentActivity() {
     private lateinit var midiManager: MidiManager
     private lateinit var midiDevices: MutableSet<MidiDeviceInfo>
     private lateinit var openedDevice: MidiDevice
-    private lateinit var openedPort: MidiInputPort
+    private lateinit var openedPort: MidiOutputPort
+    private lateinit var mr: MyMidiReceiver
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +33,9 @@ class MainActivity : ComponentActivity() {
         val listener = MidiManager.OnDeviceOpenedListener {
             println("Device opened")
             openedDevice = it
-            openedPort = openedDevice.openInputPort(0)
-            openedDevice.connectPorts(openedPort, 0)
+            openedPort = openedDevice.openOutputPort(0)
+            mr = MyMidiReceiver()
+            openedPort.connect(mr)
         }
 
         midiManager.openDevice(device, listener, null)
