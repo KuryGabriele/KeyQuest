1d8585a5cc4cae95440ea0bbc8acddb5f9b5c077
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-21 14:46:11 +0200

    Added midiPort to uiState

--- app/src/main/java/com/kuricki/keyquest/data/GameScreenUiState.kt
+++ app/src/main/java/com/kuricki/keyquest/data/GameScreenUiState.kt
@@ -1,12 +1,14 @@
 package com.kuricki.keyquest.data
 
 import android.media.midi.MidiDeviceInfo
+import android.media.midi.MidiOutputPort
 import com.kuricki.keyquest.midiStuff.MyMidiReceiver
 
 data class GameScreenUiState(
     val userName: String = "Kury",
     val currentLevel: GameLevel = GameLevel(0, "Lvl1: Ode to joy!", 0,0, 0),
     val currMidiDevice: MidiDeviceInfo? = null,
+    val midiPort: MidiOutputPort? = null,
     val mmr: MyMidiReceiver = MyMidiReceiver(),
     val currPressedKeys: MutableSet<String> = mutableSetOf<String>()
 )
