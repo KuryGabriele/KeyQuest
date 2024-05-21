43e672bc656a0d5a73dbfc2186e1eec805a0f86e
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-21 14:59:22 +0200

    Added midiSelectionOpen method, readded auto setting of device on startup

--- app/src/main/java/com/kuricki/keyquest/data/GameScreenViewModel.kt
+++ app/src/main/java/com/kuricki/keyquest/data/GameScreenViewModel.kt
@@ -18,8 +18,19 @@ class GameScreenViewModel: ViewModel() {
 
     fun start(mm: MidiManager) {
         midiManager = mm
+        //if no device selected
+        if(_uiState.value.currMidiDevice == null) {
+            //select last device
+            val devices = getDevices()
+            if(devices.isNotEmpty()) {
+                selectMidiDevice(devices.last())
+            }
+        }
     }
 
+    /**
+     * Select a new device and start listening to it
+     */
     fun selectMidiDevice(device: MidiDeviceInfo) {
         println("Selecting new device: $device")
         val listener = MidiManager.OnDeviceOpenedListener {
@@ -59,11 +70,23 @@ class GameScreenViewModel: ViewModel() {
         return devices
     }
 
+    /**
+     * Update the current pressed keys, called from the midi receiver
+     */
     fun updateCurrPressedKey(newKeys: MutableSet<String>) {
+        //Update the state with new keys
         _uiState.update {
-            println("Current keys" + it.currPressedKeys + "New keys: " + newKeys)
+            //println("Current keys" + it.currPressedKeys + "\nNew keys: " + newKeys)
             it.copy(
                 currPressedKeys = newKeys
+            )
+        }
+    }
+
+    fun midiSelectionOpen(open: Boolean) {
+        _uiState.update {
+            it.copy(
+                midiSelectionOpen = open
             )
         }
     }
