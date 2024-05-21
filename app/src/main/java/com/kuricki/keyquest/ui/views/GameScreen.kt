f5d559eb68950e5fee176e67ae60cbe73f163b5f
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-21 14:46:25 +0200

    Started work on dialog for midi device selection

--- app/src/main/java/com/kuricki/keyquest/ui/views/GameScreen.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/views/GameScreen.kt
@@ -3,18 +3,25 @@ package com.kuricki.keyquest.ui.views
 import android.media.midi.MidiManager
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.kuricki.keyquest.R
 import com.kuricki.keyquest.data.GameScreenViewModel
 import com.kuricki.keyquest.ui.components.PianoRoll
 
@@ -39,5 +46,51 @@ fun GameScreen(
                 .padding(16.dp)
         )
         PianoRoll(startNote = "C3", endNote = "G4", pressedNotes = gUiState.currPressedKeys, highlightedNotes = mutableSetOf())
+    }
+    MidiDeviceSelection(midiManager = midiManager)
+}
+
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+@Composable
+fun MidiDeviceSelection(
+    modifier: Modifier = Modifier,
+    midiManager: MidiManager,
+    gsvm: GameScreenViewModel = viewModel()
+) {
+    val gUiState by gsvm.uiState.collectAsState()
+    val midiDevices = gsvm.getDevices()
+
+    Dialog(onDismissRequest = { /*TODO*/ }) {
+        Card(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = stringResource(R.string.selMidiDevice),
+                    style = MaterialTheme.typography.labelLarge
+                )
+                midiDevices.forEach { midiDevice ->
+                    RadioButton(
+                        selected = gUiState.currMidiDevice == midiDevice,
+                        onClick = {
+                            gsvm.selectMidiDevice(midiDevice)
+                        }
+                    )
+                    Text(
+                        text = midiDevice.properties.getString("product", "asd"),
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+        }
     }
 }
