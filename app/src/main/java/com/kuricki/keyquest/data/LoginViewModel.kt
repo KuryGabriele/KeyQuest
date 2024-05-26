13e99276057134f09dc14a7f54aa88b1bd0bf064
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-05-26 13:30:23 +0200

    Implemented loginUser function

--- app/src/main/java/com/kuricki/keyquest/data/LoginViewModel.kt
+++ app/src/main/java/com/kuricki/keyquest/data/LoginViewModel.kt
@@ -1,10 +1,15 @@
 package com.kuricki.keyquest.data
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json.Default.parseToJsonElement
+import kotlinx.serialization.json.JsonObject
 
 class LoginViewModel: ViewModel() {
     private val _uiState = MutableStateFlow(LoginUiState())
@@ -27,6 +32,22 @@ class LoginViewModel: ViewModel() {
     }
 
     fun loginUser() {
-
+        println("Login", )
+        viewModelScope.launch {
+            try {
+                val json = mapOf(
+                        "username" to parseToJsonElement(_uiState.value.usrName),
+                        "hash" to parseToJsonElement(_uiState.value.psw)
+                )
+                val r = KeyQuestApi.retrofitService.getLoginSession(
+                    JsonObject(json)
+                )
+                println("Done " + r.id)
+            } catch (e: HttpException) {
+                println(e)
+            } catch (e: Exception) {
+                println(e)
+            }
+        }
     }
 }
