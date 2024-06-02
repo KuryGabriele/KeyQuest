f694c5e698c5e20679e62dc0147c46795664d20a
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-06-02 17:48:38 +0200

    Added MusicSheetOptions

--- app/src/main/java/com/kuricki/keyquest/ui/components/MusicSheet.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/components/MusicSheet.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
 data class DisplayNote(
@@ -30,17 +31,17 @@ fun MusicSheet (
             lined = true
         ),
         DisplayNote(
-            pitch = "D3",
+            pitch = "D5",
             positionY = 100f,
             lined = false
         ),
         DisplayNote(
-            pitch = "D3",
+            pitch = "D5",
             positionY = 100f,
             lined = false
         )),
+    options: MusicSheetOptions = MusicSheetOptions()
 ) {
-    val lineSpacing = 50f
     Surface(
         color = Color.White
     ) {
@@ -53,63 +54,94 @@ fun MusicSheet (
             for(i in 1..5) {
                 drawLine(
                     color = Color.Black,
-                    start = Offset(0f, i * lineSpacing),
-                    end = Offset(width, i * lineSpacing),
-                    strokeWidth = 4f
+                    start = Offset(0f, i * options.pentagramLinesSpacingScaled),
+                    end = Offset(width, i * options.pentagramLinesSpacingScaled),
+                    strokeWidth = options.pentagramLinesWidthScaled
                 )
             }
 
             // foreach note
-            var noteOffset = 100f //position of the notes
+            var noteOffset = options.notesOffsetScaled //position of the notes
             for(note in notes) {
                 //draw it as a circle
                 drawCircle(
-                    color = Color.Black,
-                    radius = 20f,
+                    color = options.notesColor,
+                    radius = options.notesRadiusScaled,
                     center = Offset(noteOffset, note.positionY)
                 )
                 if(note.lined) {
                     //draw horizontal line across note
                     drawLine(
-                        color = Color.Black,
+                        color = options.notesColor,
                         start = Offset(noteOffset-35f, note.positionY),
                         end = Offset(noteOffset+35f, note.positionY),
-                        strokeWidth = 10f
+                        strokeWidth = options.notesLinedWidthScaled
                     )
                 }
 
                 // check if vertical line can be drawn up
-                if(note.positionY < 200) {
+                val verticalLineOffset = noteOffset + options.verticalLineOffsetScaled
+                if(note.positionY < options.maxHeightForVerticalLineUpScaled) {
                     // draw vertical line downwards from note
                     drawLine(
-                        color = Color.Black,
-                        start = Offset(noteOffset+18f, note.positionY),
-                        end = Offset(noteOffset+18f, note.positionY+125f),
-                        strokeWidth = 5f
+                        color = options.notesColor,
+                        start = Offset(verticalLineOffset, note.positionY),
+                        end = Offset(verticalLineOffset, note.positionY + options.verticalLineLengthScaled),
+                        strokeWidth = options.verticalLineWidthScaled
                     )
                 } else {
                     //draw vertical line upwards from note
                     drawLine(
-                        color = Color.Black,
-                        start = Offset(noteOffset+18f, note.positionY),
-                        end = Offset(noteOffset+18f, note.positionY-125f),
-                        strokeWidth = 5f
+                        color = options.notesColor,
+                        start = Offset(verticalLineOffset, note.positionY),
+                        end = Offset(verticalLineOffset, note.positionY - options.verticalLineLengthScaled),
+                        strokeWidth = options.verticalLineWidthScaled
                     )
                 }
 
                 // draw pitch text
                 drawContext.canvas.nativeCanvas.drawText (
                     note.pitch,
-                    noteOffset - 20f,
-                    375f,
+                    noteOffset - options.notesRadiusScaled,
+                    options.pitchNotesHeightScaled,
                     Paint().asFrameworkPaint().apply {
                         color = Color.Black.toArgb()
-                        textSize = 15.sp.toPx()
+                        textSize = options.pitchNotesSizeScaled.toPx()
                     }
                 )
 
-                noteOffset += 100f
+                noteOffset += options.notesOffsetScaled
             }
         }
     }
+}
+
+data class MusicSheetOptions(
+    val sizeScale: Float = 1f,
+    val pentagramLinesColor: Color = Color.Black,
+    val pentagramLinesWidth: Float = 4f,
+    val pentagramLinesSpacing: Float = 50f,
+    val notesColor: Color = Color.Black,
+    val notesPitchColor: Color = Color.Black,
+    val notesOffset: Float = 100f,
+    val notesRadius: Float = 20f,
+    val notesLinesWidth: Float = 10f,
+    val maxHeightForVerticalLineUp: Float = 200f,
+    val verticalLineLength: Float = 125f,
+    val verticalLineOffset: Float = 18f,
+    val verticalLineWidth: Float = 5f,
+    val pitchNotesHeight: Float = 375f,
+    val pitchNotesSize: TextUnit = 15.sp
+) {
+    val pentagramLinesWidthScaled = pentagramLinesWidth * sizeScale
+    val pentagramLinesSpacingScaled = pentagramLinesSpacing * sizeScale
+    val notesOffsetScaled = notesOffset * sizeScale
+    val notesRadiusScaled = notesRadius * sizeScale
+    val notesLinedWidthScaled = notesLinesWidth * sizeScale
+    val maxHeightForVerticalLineUpScaled = maxHeightForVerticalLineUp * sizeScale
+    val verticalLineLengthScaled = verticalLineLength * sizeScale
+    val verticalLineOffsetScaled = verticalLineOffset * sizeScale
+    val verticalLineWidthScaled = verticalLineWidth * sizeScale
+    val pitchNotesHeightScaled = pitchNotesHeight * sizeScale
+    val pitchNotesSizeScaled = pitchNotesSize * sizeScale
 }
