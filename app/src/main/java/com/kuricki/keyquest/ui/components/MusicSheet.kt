5f506be8dc44dee13b2fc5466e203cbc0e325f0d
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-06-02 14:24:15 +0200

    Added white background, added line support on notes

--- app/src/main/java/com/kuricki/keyquest/ui/components/MusicSheet.kt
+++ app/src/main/java/com/kuricki/keyquest/ui/components/MusicSheet.kt
@@ -2,16 +2,18 @@ package com.kuricki.keyquest.ui.components
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 data class DisplayNote(
     val pitch: String,
     val positionX: Float,
-    val positionY: Float
+    val positionY: Float,
+    val lined: Boolean
 )
 
 
@@ -19,36 +21,56 @@ data class DisplayNote(
 @Composable
 fun MusicSheet (
     modifier: Modifier = Modifier,
-    notes: List<DisplayNote> = listOf(DisplayNote(
-        pitch = "C4",
-        positionX = 100f,
-        positionY = 300f
-    )),
+    notes: List<DisplayNote> = listOf(
+        DisplayNote(
+            pitch = "C4",
+            positionX = 100f,
+            positionY = 300f,
+            lined = true
+        ),
+        DisplayNote(
+            pitch = "D4",
+            positionX = 200f,
+            positionY = 270f,
+            lined = false
+        )),
 ) {
     val lineSpacing = 50f
-    val primaryColor = MaterialTheme.colorScheme.primary
-    val tertiaryColor = MaterialTheme.colorScheme.tertiary
-    Canvas(
-        modifier= Modifier.fillMaxSize()
+    Surface(
+        color = Color.White
     ) {
-        val width = size.width
-        val height = size.height
+        Canvas(
+            modifier= Modifier.fillMaxSize(),
+        ) {
+            val width = size.width
+            val height = size.height
 
-        for(i in 1..5) {
-            drawLine(
-                color = tertiaryColor,
-                start = Offset(0f, i * lineSpacing),
-                end = Offset(width, i * lineSpacing),
-                strokeWidth = 4f
-            )
-        }
+            for(i in 1..5) {
+                drawLine(
+                    color = Color.Black,
+                    start = Offset(0f, i * lineSpacing),
+                    end = Offset(width, i * lineSpacing),
+                    strokeWidth = 4f
+                )
+            }
 
-        for(note in notes) {
-            drawCircle(
-                color = primaryColor,
-                radius = 20f,
-                center = Offset(note.positionX, note.positionY)
-            )
+            for(note in notes) {
+                drawCircle(
+                    color = Color.Black,
+                    radius = 20f,
+                    center = Offset(note.positionX, note.positionY)
+                )
+                if(note.lined) {
+                    //draw horizontal line across note
+                    drawLine(
+                        color = Color.Black,
+                        start = Offset(note.positionX-35f, note.positionY),
+                        end = Offset(note.positionX+35f, note.positionY),
+                        strokeWidth = 10f
+                    )
+                }
+            }
         }
     }
+
 }
