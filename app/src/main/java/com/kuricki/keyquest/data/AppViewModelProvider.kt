2c1f024583bf72d761dcd2422f55ba1b59804567
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-06-04 17:14:32 +0200

    Added AppViewModelProvider

---
+++ app/src/main/java/com/kuricki/keyquest/data/AppViewModelProvider.kt
@@ -0,0 +1,26 @@
+package com.kuricki.keyquest.data
+
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.kuricki.keyquest.KeyquestApplication
+
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            LoginViewModel(
+                keyquestApplication().container.userSessionRepository
+            )
+        }
+
+        initializer {
+            LevelSelectViewModel(
+                keyquestApplication().container.gameLevelRepository
+            )
+        }
+    }
+}
+
+fun CreationExtras.keyquestApplication(): KeyquestApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as KeyquestApplication)
