4c023c16026d358c49ab273f4c150a211866fc4e
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-06-04 15:54:09 +0200

    Added userSessinRepository to AppContainer

--- app/src/main/java/com/kuricki/keyquest/db/AppContainer.kt
+++ app/src/main/java/com/kuricki/keyquest/db/AppContainer.kt
@@ -5,11 +5,16 @@ import android.content.Context
 
 interface AppContainer {
     val gameLevelRepository: GameLevelRepository
+    val userSessionRepository: UserSessionRepository
 }
 
 class AppDataContainer(private val context: Context): AppContainer {
     override val gameLevelRepository: GameLevelRepository by lazy {
         OfflineGameLevelRepository(KeyquestDatabase.getDatabase(context).gameLevelDao())
+    }
+
+    override val userSessionRepository: UserSessionRepository by lazy {
+        OfflineUserSessionRepository(KeyquestDatabase.getDatabase(context).userSessionDao())
     }
 
 }
