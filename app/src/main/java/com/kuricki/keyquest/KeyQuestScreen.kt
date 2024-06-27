6ec598803eab73a09a968a5ca7c085bc2c00de55
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-06-27 14:37:37 +0200

    Added slide animation on navigator

--- app/src/main/java/com/kuricki/keyquest/KeyQuestScreen.kt
+++ app/src/main/java/com/kuricki/keyquest/KeyQuestScreen.kt
@@ -4,9 +4,8 @@ import android.media.midi.MidiManager
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.SlideTransition
 import com.kuricki.keyquest.ui.views.LoginScreen
 
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
@@ -14,8 +13,7 @@ import com.kuricki.keyquest.ui.views.LoginScreen
 fun KeyQuestApp(
     midiManager: MidiManager,
 ) {
-    val context = LocalContext.current
-    val navigator = LocalNavigator.current
-
-    Navigator(LoginScreen())
+    Navigator(LoginScreen()) { n ->
+        SlideTransition(n)
+    }
 }
