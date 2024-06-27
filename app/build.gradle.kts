a23abfd0b73dba73dfdb6be9fc29c39d18d4d5c9
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-06-27 20:50:13 +0200

    Enabled edge to edge

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.tools.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/kuricki/keyquest/MainActivity.kt
+++ app/src/main/java/com/kuricki/keyquest/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.kuricki.keyquest
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.ui.platform.LocalContext
 import com.kuricki.keyquest.ui.theme.KeyQuestTheme
 
 class MainActivity : ComponentActivity() {
 
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Make status bar transparent
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            ),
+            statusBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
         super.onCreate(savedInstanceState)
         setContent {
+            val context = LocalContext.current
+
             KeyQuestTheme {
                 KeyQuestApp()
             }
