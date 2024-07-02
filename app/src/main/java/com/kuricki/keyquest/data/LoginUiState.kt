6520a31f7a32bb54faf4c70b66af4e649a57c896
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-07-02 14:43:34 +0200

    Added psw2 in loginUiState for registration

--- app/src/main/java/com/kuricki/keyquest/data/LoginUiState.kt
+++ app/src/main/java/com/kuricki/keyquest/data/LoginUiState.kt
@@ -3,6 +3,7 @@ package com.kuricki.keyquest.data
 data class LoginUiState(
     val usrName: String = "", // username
     val psw: String = "", // password
+    val psw2: String = "", // repeat password
     val error: String? = null, //error message
     val checkSession: Boolean = true //should check be checked
 )
