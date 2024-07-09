8ffdb94de2e08c6112e3b4dcd56474d80255e4a1
Kuricki Gabriele <35193509+KuryGabriele@users.noreply.github.com>
2024-07-09 11:45:44 +0200

    Added update of score on summary screen

--- app/src/main/java/com/kuricki/keyquest/data/SummaryScreenModel.kt
+++ app/src/main/java/com/kuricki/keyquest/data/SummaryScreenModel.kt
@@ -1,28 +1,52 @@
 package com.kuricki.keyquest.data
 
+import android.content.Context
 import androidx.compose.runtime.asIntState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import com.kuricki.keyquest.db.GameLevel
 import com.kuricki.keyquest.db.GameLevelRepository
+import com.kuricki.keyquest.db.UserSession
+import com.kuricki.keyquest.utils.isOnline
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json.Default.parseToJsonElement
+import kotlinx.serialization.json.JsonObject
 
-class SummaryScreenModel(val lvl: GameLevel, val score: Int, val repository: GameLevelRepository): ScreenModel {
-    private val _bestScore = mutableStateOf(lvl.bestScore)
+class SummaryScreenModel(private val loginSession: UserSession, private val context: Context, private val lvl: GameLevel, private val score: Int, private val repository: GameLevelRepository): ScreenModel {
+    private val _bestScore = mutableIntStateOf(lvl.bestScore)
     val bestScore = _bestScore.asIntState()
 
     init {
         screenModelScope.launch {
             repository.getLevelById(lvl.id).collect {
-                if (it == null || it.bestScore < _bestScore.value) {
+                if (it == null || it.bestScore < _bestScore.intValue) {
                     //if no best score is present or
                     //the new score is higher update
                     repository.saveLevel(lvl)
+
+                    if(isOnline(context)) {
+                        //update best score on api
+                        try {
+                            val json = mapOf(
+                                "lId"  to parseToJsonElement(lvl.id.toString()),
+                                "score" to parseToJsonElement(score.toString())
+                            )
+                            KeyQuestApi.retrofitService.submitLevelScore(
+                                loginSession.token,
+                                JsonObject(json)
+                            )
+                        } catch (e: retrofit2.HttpException) {
+                            println("Error saving score to api")
+                            println(e)
+                        } catch (e: Exception) {
+                            println(e)
+                        }
+                    }
                 } else {
-                    if(it.bestScore > _bestScore.value){
+                    if(it.bestScore > _bestScore.intValue){
                         //if not best score
-                        _bestScore.value = it.bestScore
+                        _bestScore.intValue = it.bestScore
                     }
                 }
             }
